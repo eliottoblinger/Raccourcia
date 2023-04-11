4c80d8b90e808123f23fdba5dc62d90f96449ec0
Eliott <eliott.oblinger7@etu.univ-lorraine.fr>
2023-04-11 21:40:47 +0200

    remove isWhite function

--- src/background/utils/color.js
+++ src/background/utils/color.js
@@ -1,17 +1,5 @@
-const rgba2hex = (rgba) => `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`
-
-function isWhite(str) {
-    // fiddle this value to set stricter rules for what is white.
-    var whiteLimit = 100,
-        r,g,b;
-
-    r = parseInt("0x"+str.substring(1,3));
-    g = parseInt("0x"+str.substring(3,5));
-    b = parseInt("0x"+str.substring(5,7));
-    if(r < whiteLimit || b < whiteLimit || g < whiteLimit) {
-        return false;
-    }
-    return true;
+const rgba2hex = (rgba) => {
+    return `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`;
 }
 
 const findHtmlColors = () => {
